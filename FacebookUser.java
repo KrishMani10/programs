e54fee9679614f795e34a0cc0711088e45ccea15
KrishMani10 <95026713+KrishMani10@users.noreply.github.com>
2022-01-07 13:07:00 +0530

    Update FacebookUser.java

--- FacebookUser.java
+++ FacebookUser.java
@@ -1,21 +1,22 @@
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FacebookUser {
+class FacebookUser {
 
     String userName;
     String profileName;
-    String gender;
-    LocalDate  age;
+    Gender gender;
+    LocalDate age;
     int dob;
     String currentLocation;
     String workingPlace;
     String schooling;
     String college;
+    List<FacebookUser> friends = new ArrayList<>();
 
 
-    FacebookUser(String userName, String profileName, String gender, LocalDate age, int dob, String currentLocation, String workingPlace, String schooling, String college){
+    FacebookUser(String userName, String profileName, Gender gender,LocalDate age,int dob, String currentLocation, String workingPlace, String schooling, String college){
         this.userName = userName;
         this.profileName = profileName;
         this.gender = gender;
@@ -25,16 +26,24 @@ public class FacebookUser {
         this.workingPlace = workingPlace;
         this.schooling = schooling;
         this.college = college;
+    }
 
+
+    void addFriend( FacebookUser facebookUser ){
+        friends.add(facebookUser);
     }
 
     void printData(){
         String line = System.lineSeparator();
         System.out.println("User_Id : " +userName+ "" +line+ "Profile_Name : " +profileName+  "\t" + "||" + "\t" + "Gender : " +gender+
-                "\t" + "||" + "\t" +"Date of birth : " +dob+ "\t" + "||" + "\t" + "Age :" +age+ "" +line+ "Current location : " +currentLocation+ "\t" + "||" +
-                "\t" + "Current Working place :" +workingPlace+ "" +line+ "Schooling place : " +schooling+ "\t" + "||" + "\t" + "College : " +college  );
+                "\t" + "||" + "\t" +"Date of birth : " +age+ "\t" + "||" + "\t" + "Age :" +dob+ "" +line+ "Current location : " +currentLocation+ "\t" + "||" +
+                "\t" + "Current Working place :" +workingPlace+ "" +line+ "Schooling place : " +schooling+ "\t" + "||" + "\t" + "College : " +college);
         System.out.println();
-
+        System.out.println("List of friends : ");
+        for (FacebookUser friend : friends) {
+            System.out.println(friend.profileName);
+        }
+        System.out.println();
     }
 
     @Override
@@ -50,4 +59,10 @@ public class FacebookUser {
         return false;
     }
 
+}
+
+enum Gender {
+    MALE,
+    FEMALE,
+    UNSPECIFIED
 }
