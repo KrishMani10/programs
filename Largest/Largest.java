c7d006426f08cd0b500f123524e61bd74b53b687
KrishMani10 <95026713+KrishMani10@users.noreply.github.com>
2022-01-13 19:14:01 +0530

    Add files via upload

---
+++ Largest/Largest.java
@@ -0,0 +1,29 @@
+package mani;
+
+import java.util.Scanner;
+
+public class Largest {
+        public static void main(String[] args) {
+            int a;
+            int t=0;
+            Scanner s = new Scanner(System.in);
+            a = s.nextInt();
+            int arr[] = new int[a];
+            for(int i=0;i<a;i++){
+                arr[i] = s.nextInt();
+            }
+            System.out.println("Array elements are:" );
+            for(int i=0;i<a;i++){
+                System.out.print(arr[i] +"\t");
+            }
+            t = arr[0];
+            for(int i=0;i<a;i++){
+
+                if(arr[i]>t){
+                    t = arr[i];
+                }
+            }
+            System.out.println("LArgest number in a array:" +t);
+        }
+
+}
